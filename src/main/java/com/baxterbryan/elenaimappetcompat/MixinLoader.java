4941731945dca0704119c6c1f48f4c62f29d33e2
Ryan Shah <ryan.k.shah@gmail.com>
2025-05-01 11:34:26 +0100

    Fixed compatibility issue between Elenai Dodge 2 and Mappet

---
+++ src/main/java/com/baxterbryan/elenaimappetcompat/MixinLoader.java
@@ -0,0 +1,16 @@
+package com.baxterbryan.elenaimappetcompat;
+
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MixinLoader implements ILateMixinLoader {
+    @Override
+    public List<String> getMixinConfigs() {
+        ArrayList<String> configs = new ArrayList<>();
+        configs.add("mixins.elenaimappetcompat.json");
+        return configs;
+    }
+}
